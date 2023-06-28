84b29ca862ed29117df061d65f9945f08822ec22
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2023-06-28 18:32:14 +0700

    Added Database

--- src/Backend/LZW/models.py
+++ src/Backend/LZW/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class History(models.Model):
+    name = models.CharField(max_length=25)
+    user_input = models.TextField()
+    output = models.TextField()
--- src/Backend/LZW/views.py
+++ src/Backend/LZW/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from . import Encoder, Decoder, Utils
+from .models import History
+
 
 ascii_count = 256
 ascii_range = range(0, ascii_count)
@@ -12,21 +14,40 @@ def index(request):
     dec_res = None
     choice1 = 'decimal'
     choice2 = 'decimal'
+    checked = False
 
     if (request.method == 'POST'):
         choice1 = request.POST['format1']
         choice2 = request.POST['format2']
-        if (request.POST['chars'] != ''):
-            enc_res = Encoder.encode(request.POST['chars'], ascii_list)
+        chars = request.POST['chars']
+        code = request.POST['code']
+        if (chars != ''):
+            enc_res = Encoder.encode(chars, ascii_list)
             if (request.POST['format1'] == 'binary'):
                 enc_res = [Utils.Utils.decimalToBinary(x) for x in enc_res]
             enc_res = ' '.join(map(str, enc_res))
+            if ('saveCheckbox' in request.POST):
+                history1 = History()
+                history1.name = "Compression"
+                history1.user_input = chars
+                history1.output = enc_res
+                history1.save()
+                checked = True
 
-        if (request.POST['code'] != ''):
-            codeList = request.POST['code'].split()
+        if (code != ''):
+            codeList = code.split()
             if (request.POST['format2'] == 'binary'):
                 codeList = [Utils.Utils.binaryToDecimal(x) for x in codeList]
             codeList = [int(i) for i in codeList]
             dec_res = Decoder.decode(codeList, ascii_list)
-            
-    return render(request, 'index.html', {'enc_result': enc_res, 'dec_result': dec_res, 'prevChoice1':choice1, 'prevChoice2':choice2})
+            if ('saveCheckbox' in request.POST):
+                history2 = History()
+                history2.name = "Decompression"
+                history2.user_input = code
+                history2.output = dec_res
+                history2.save()
+                checked = True
+
+    histories = History.objects.all()
+
+    return render(request, 'index.html', {'enc_result': enc_res, 'dec_result': dec_res, 'prevChoice1':choice1, 'prevChoice2':choice2, 'checked':checked, 'histories': histories})
